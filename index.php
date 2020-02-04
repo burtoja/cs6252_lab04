57d7467820aa340827cb9dcbacc73b1c3d6846d9
J. Allen Burton <jburton6@my.westga.edu>
2020-02-04 04:42:56 -0500

    Added 'Clear All' functionality
    Clear session data, clean up session ID, and delete session cookie.

--- index.php
+++ index.php
@@ -16,6 +16,16 @@ $errors = array();
 
 switch($action) {
     case 'Clear All':
+    	$_SESSION = array();
+    	session_destroy();
+    	$name = session_name();
+    	$expire = strtotime('-1 year');
+    	$params = session_get_cookie_params();
+    	$path = $params['path'];
+    	$domain = $params['domain'];
+    	$secure = $params['secure'];
+    	$httponly = $params['httponly'];
+    	setcookie($name, '', $expire, $path, $domain, $secure, $httponly);
         break;
     case 'Add Task':
         $new_task = filter_input(INPUT_POST, 'newtask');
